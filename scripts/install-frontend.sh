5b6d6ff8e9b19007959c1e3f245379f4b9b36623
blackd6 <blackd6@wwu.edu>
2023-11-07 23:21:31 -0800

    Preparing for writing frontend install script

---
+++ scripts/install-frontend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "- Installing Webcomic frontend...-"
+
+#setup variables
+script_directory=$(dirname "$0")
+cd "$script_directory/.." || exit
+
---
+++ scripts/setupUser.sql
@@ -0,0 +1,2 @@
+CREATE USER 'backend'@'localhost' IDENTIFIED BY 'PeanutButter69!';
+GRANT ALL PRIVILEGES ON *.* TO 'backend'@'localhost' WITH GRANT OPTION;

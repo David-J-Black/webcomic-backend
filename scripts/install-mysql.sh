e24e9850930ae20bb82099cc4e37dd8715af9d43
David Black <davidblacktheemployee@gmail.com>
2023-11-08 04:45:48 +0000

    Set up the theoretical mysql script

--- scripts/install-mysql.sh
+++ scripts/install-mysql.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install mysql-server
+sudo apt-get update -y
+sudo apt-get install -y mysql-server
+
+sudo mysql_secure_installation utility
+
+#!/bin/bash
+
+# MySQL connection parameters
+MYSQL_USER="root"
+MYSQL_PASSWORD="PeanutButter"
+MYSQL_DATABASE="your_database"
+
+# MySQL command to run
+MYSQL_COMMAND="SELECT * FROM your_table;"
+
+# Run the MySQL command
+sudo mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -D "$MYSQL_DATABASE" -e "$MYSQL_COMMAND"

0d301d8e1614d7acd222e7aa3cdd28a5c423cea5
David Black <davidblacktheemployee@gmail.com>
2023-11-08 06:23:50 +0000

    Added setting up root with a password works

--- scripts/install-mysql.sh
+++ scripts/install-mysql.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -y
 sudo apt-get install -y mysql-server
 
-sudo mysql_secure_installation utility
+# sudo mysql_secure_installation utility
 
 #!/bin/bash
 
@@ -13,7 +13,11 @@ MYSQL_PASSWORD="PeanutButter"
 MYSQL_DATABASE="your_database"
 
 # MySQL command to run
-MYSQL_COMMAND="SELECT * FROM your_table;"
+# MYSQL_COMMAND="SELECT * FROM your_table;"
 
 # Run the MySQL command
-sudo mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -D "$MYSQL_DATABASE" -e "$MYSQL_COMMAND"
+# sudo mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -D "$MYSQL_DATABASE" -e "$MYSQL_COMMAND"
+
+sudo mysql
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'PeanutButter' WITH GRANT OPTION;
+FLUSH PRIVILEGES;

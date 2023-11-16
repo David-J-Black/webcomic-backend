2f239d03e259b9f4f2974c2dc01ea43f67ea23ea
David Black <davidblacktheemployee@gmail.com>
2023-11-16 05:49:46 +0000

    nginx install stopped working so i removed a variable

--- scripts/install-nginx.sh
+++ scripts/install-nginx.sh
@@ -4,16 +4,18 @@ script_location=$(dirname "$0")
 cd "$script_location/.." || Exit
 current_location=$(pwd)
 nginx_config_location="$current_location/filesToMoveUponInstallation/nginxWebcomic.conf"
-sconfig_destination="/etc/nginx/nginx.conf"
 
 echo "| Installing Nginx...|"
 
 sudo apt install nginx
 
-sudo cp -f "$nginx_config_location" "$config_destination" 
+echo 'Moving nginx file...'
+# sudo cp filesToMoveUponInstallation/nginxWebcomic.conf /etc/nginx/nginx.conf
+sudo cp -f "$nginx_config_location" /etc/nginx/nginx.conf
+sudo
 
 sudo systemctl enable nginx
-sudo systemctl start nginx
+sudo systemctl restart nginx
 sudo systemctl status nginx.service 
 
 sudo nginx -s reload
